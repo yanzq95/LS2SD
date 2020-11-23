7501305549b92710f5064c8cfff6218134fbfb07
anonymoustbd <74827816+anonymoustbd@users.noreply.github.com>
2020-11-23 21:37:24 +0800

    Add files via upload

---
+++ 2x/LS2SD/all_attention.py
@@ -0,0 +1,158 @@
+import torch
+import torch.nn as nn
+from torch.nn import Module, Conv2d, Parameter, Softmax
+
+__all__ = ['PAM_Module', 'CAM_Module']
+
+
+class PAM_Module(Module):
+    """ Position attention module"""
+    def __init__(self, in_dim, height, width, kernel_narrow=1):
+        super(PAM_Module, self).__init__()
+        self.chanel_in = in_dim
+        # self.kernel_narrow = kernel_narrow
+
+        self.query_conv = Conv2d(in_channels=in_dim, out_channels=in_dim, kernel_size=1)
+        self.key_conv = Conv2d(in_channels=in_dim, out_channels=in_dim, kernel_size=1)
+        self.value_conv = Conv2d(in_channels=in_dim, out_channels=in_dim, kernel_size=1)
+
+        self.query_pool = torch.nn.AvgPool2d(kernel_size=(kernel_narrow, width), stride=1, padding=((kernel_narrow-1)//2, 0))
+        self.key_pool = torch.nn.AvgPool2d(kernel_size=(height, kernel_narrow), stride=1, padding=(0, (kernel_narrow-1)//2))
+
+        self.gamma = Parameter(torch.zeros(1))
+        self.softmax = Softmax(dim=-1)
+
+    def forward(self, x):
+        """
+            inputs :
+                x : input feature maps( B X C X H X W) B * 64 * 256 * 1216
+            returns :
+                out : attention value + input feature
+                attention: B X (HxW) X (HxW)
+        """
+        m_batchsize, C, height, width = x.size()
+        proj_query = self.query_conv(x)
+        proj_query_pool = self.query_pool(proj_query)
+
+        proj_key = self.key_conv(x)
+        proj_key_pool = self.key_pool(proj_key)
+
+        energy = proj_query_pool @ proj_key_pool
+        energy_reshape = energy.view(m_batchsize, C, height * width)
+        attention = self.softmax(energy_reshape)
+        attention_reshape = attention.view(m_batchsize, C, height, width)
+
+        proj_value = self.value_conv(x)
+        out = proj_value * attention_reshape
+        out = self.gamma*out + x
+
+        return out
+
+
+class CAM_Module(Module):
+    """ Channel attention module"""
+    def __init__(self, in_dim, height=None, width=None):
+        super(CAM_Module, self).__init__()
+        self.chanel_in = in_dim
+
+        self.query_conv = Conv2d(in_channels=in_dim, out_channels=in_dim, kernel_size=1)
+        self.query_pool = torch.nn.AdaptiveAvgPool2d(1)
+
+        self.key_conv = Conv2d(in_channels=in_dim, out_channels=in_dim, kernel_size=1)
+        self.key_pool = torch.nn.AdaptiveAvgPool2d(1)
+
+        self.value_conv = Conv2d(in_channels=in_dim, out_channels=in_dim, kernel_size=1)
+
+        self.gamma = Parameter(torch.zeros(1))
+        self.softmax  = Softmax(dim=-1)
+    def forward(self,x):
+        """
+            inputs :
+                x : input feature maps( B X C X H X W)
+            returns :
+                out : attention value + input feature
+                attention: B X C X C
+        """
+        m_batchsize, C, height, width = x.size()
+
+        proj_query = self.query_conv(x)
+        proj_query_pool = self.query_pool(proj_query)
+        proj_query_pool_reshape = proj_query_pool.view(m_batchsize, C, -1)
+
+        proj_key = self.key_conv(x)
+        proj_key_pool = self.key_pool(proj_key)
+        proj_key_pool_reshape = proj_key_pool.view(m_batchsize, C, -1).permute(0, 2, 1)
+
+        energy = torch.bmm(proj_query_pool_reshape, proj_key_pool_reshape)
+        energy_reshape = energy.view(m_batchsize, -1)
+
+        attention = self.softmax(energy_reshape)
+        attention_reshape = attention.view(m_batchsize, C, C)
+
+        proj_value = self.value_conv(x)
+        proj_value_reshape = proj_value.view(m_batchsize, C, -1)
+
+        out = torch.bmm(attention_reshape, proj_value_reshape)
+        out = out.view(m_batchsize, C, height, width)
+        out = self.gamma*out + x
+
+        return out
+
+
+class PAM_and_CAM(Module):
+    def __init__(self, in_dim, height, width):
+        super(PAM_and_CAM, self).__init__()
+        self.chanel_in = in_dim
+        self.pa = PAM_Module(in_dim, height, width)
+        self.ca = CAM_Module(in_dim, height, width)
+        self.conv_pa = conv_bn_relu(in_dim, in_dim, 3, 1, 1, bn=False, relu=False)
+        self.conv_ca = conv_bn_relu(in_dim, in_dim, 3, 1, 1, bn=False, relu=False)
+
+    def forward(self, x):
+
+        pa_att = self.pa(x)
+        pa_learning = self.conv_pa(pa_att)
+
+        ca_att = self.ca(x)
+        ca_learning = self.conv_ca(ca_att)
+
+        pa_sum_ca = pa_learning + ca_learning
+
+        return pa_sum_ca
+
+
+def init_weights(m):
+    if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+        m.weight.data.normal_(0, 1e-3)
+        if m.bias is not None:
+            m.bias.data.zero_()
+    elif isinstance(m, nn.ConvTranspose2d):
+        m.weight.data.normal_(0, 1e-3)
+        if m.bias is not None:
+            m.bias.data.zero_()
+    elif isinstance(m, nn.BatchNorm2d):
+        m.weight.data.fill_(1)
+        m.bias.data.zero_()
+
+def conv_bn_relu(in_channels, out_channels, kernel_size, \
+        stride=1, padding=0, bn=True, relu=True):
+    bias = not bn
+    layers = []
+    layers.append(
+        nn.Conv2d(in_channels,
+                  out_channels,
+                  kernel_size,
+                  stride,
+                  padding,
+                  bias=bias))
+    if bn:
+        layers.append(nn.BatchNorm2d(out_channels))
+    if relu:
+        layers.append(nn.LeakyReLU(0.2, inplace=True))
+    layers = nn.Sequential(*layers)
+
+    # initialize the weights
+    for m in layers.modules():
+        init_weights(m)
+
+    return layers
---
+++ 4x/LS2SD/model.py
@@ -0,0 +1,360 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision.models import resnet
+from all_attention import PAM_and_CAM
+
+def init_weights(m):
+    if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+        m.weight.data.normal_(0, 1e-3)
+        if m.bias is not None:
+            m.bias.data.zero_()
+    elif isinstance(m, nn.ConvTranspose2d):
+        m.weight.data.normal_(0, 1e-3)
+        if m.bias is not None:
+            m.bias.data.zero_()
+    elif isinstance(m, nn.BatchNorm2d):
+        m.weight.data.fill_(1)
+        m.bias.data.zero_()
+
+def conv_bn_relu(in_channels, out_channels, kernel_size, \
+        stride=1, padding=0, bn=True, relu=True):
+    bias = not bn
+    layers = []
+    layers.append(
+        nn.Conv2d(in_channels,
+                  out_channels,
+                  kernel_size,
+                  stride,
+                  padding,
+                  bias=bias))
+    if bn:
+        layers.append(nn.BatchNorm2d(out_channels))
+    if relu:
+        layers.append(nn.LeakyReLU(0.2, inplace=True))
+    layers = nn.Sequential(*layers)
+
+    # initialize the weights
+    for m in layers.modules():
+        init_weights(m)
+
+    return layers
+
+def convt_bn_relu(in_channels, out_channels, kernel_size, \
+        stride=1, padding=0, output_padding=0, bn=True, relu=True):
+    bias = not bn
+    layers = []
+    layers.append(
+        nn.ConvTranspose2d(in_channels,
+                           out_channels,
+                           kernel_size,
+                           stride,
+                           padding,
+                           output_padding,
+                           bias=bias))
+    if bn:
+        layers.append(nn.BatchNorm2d(out_channels))
+    if relu:
+        layers.append(nn.LeakyReLU(0.2, inplace=True))
+    layers = nn.Sequential(*layers)
+
+    # initialize the weights
+    for m in layers.modules():
+        init_weights(m)
+
+    return layers
+
+class field_transform(nn.Module):
+    def __init__(self, c, h, w):
+        super(field_transform, self).__init__()
+
+        self.c = c
+        self.h = h
+        self.w = w
+
+        self.conv_for_dc_add_sr = conv_bn_relu(c, c, 1)
+        self.att = PAM_and_CAM(c, h, w)
+
+        self.dc_alpha = nn.Parameter(torch.zeros(1))
+        self.sr_beta = nn.Parameter(torch.zeros(1))
+
+    def forward(self, dc_feature, sr_feature):
+
+        dc = dc_feature
+        sr = sr_feature
+        dc_add_sr = dc_feature + sr_feature
+        out = self.conv_for_dc_add_sr(dc_add_sr)
+        att = self.att(out)
+        dc_out = dc + self.dc_alpha * att
+        sr_out = sr + self.sr_beta * att
+
+        return dc_out, sr_out
+
+class DepthCompletionNet(nn.Module):
+    def __init__(self, args):
+        assert (
+            args.layers in [18, 34, 50, 101, 152]
+        ), 'Only layers 18, 34, 50, 101, and 152 are defined, but got {}'.format(args.layers)
+        super(DepthCompletionNet, self).__init__()
+        self.modality = args.input
+
+
+        channels = 16
+        self.conv1_d = conv_bn_relu(1,
+                                    # channels,
+                                    16,
+                                    kernel_size=3,
+                                    stride=1,
+                                    padding=1)
+
+        channels = 48
+        self.conv1_img = conv_bn_relu(3,
+                                      # channels,
+                                      64,
+                                      kernel_size=3,
+                                      stride=1,
+                                      padding=1)
+        self.conv1_img2 = conv_bn_relu(64,
+                                      # channels,
+                                      64,
+                                      kernel_size=1,
+                                      stride=1,
+                                      padding=0)
+
+        self.conv2_img = conv_bn_relu(64,
+                                      # channels,
+                                      64,
+                                      kernel_size=1,
+                                      stride=1,
+                                      padding=0)
+        self.conv2_img2 = conv_bn_relu(64,
+                                      # channels,
+                                      64,
+                                      kernel_size=3,
+                                      stride=2,
+                                      padding=1)
+        self.conv3_img = conv_bn_relu(64,
+                                      # channels,
+                                      48,
+                                      kernel_size=1,
+                                      stride=1,
+                                      padding=0)
+        self.conv3_img2 = conv_bn_relu(48,
+                                      # channels,
+                                      48,
+                                      kernel_size=3,
+                                      stride=2,
+                                      padding=1)
+
+        pretrained_model = resnet.__dict__['resnet{}'.format(
+            args.layers)](pretrained=args.pretrained)
+        if not args.pretrained:
+            pretrained_model.apply(init_weights)
+        #self.maxpool = pretrained_model._modules['maxpool']
+        self.conv2 = pretrained_model._modules['layer1']
+        self.conv3 = pretrained_model._modules['layer2']
+        self.conv4 = pretrained_model._modules['layer3']
+        self.conv5 = pretrained_model._modules['layer4']
+        del pretrained_model  # clear memory
+
+        # define number of intermediate channels
+        num_channels = 512
+
+        # decoding layers
+        kernel_size = 3
+        stride = 2
+        self.convt4 = convt_bn_relu(in_channels=512,
+                                    out_channels=256,
+                                    kernel_size=kernel_size,
+                                    stride=stride,
+                                    padding=1,
+                                    output_padding=1)
+        self.srconvt4 = convt_bn_relu(512, 256, kernel_size, stride, 1, 1)
+
+        self.convt3 = convt_bn_relu(in_channels=256,
+                                    out_channels=128,
+                                    kernel_size=kernel_size,
+                                    stride=stride,
+                                    padding=1,
+                                    output_padding=1)
+        self.srconvt3 = convt_bn_relu(256, 128, kernel_size, stride, 1, 1)
+
+        self.convt2 = convt_bn_relu(in_channels=128,
+                                    out_channels=64,
+                                    kernel_size=kernel_size,
+                                    stride=stride,
+                                    padding=1,
+                                    output_padding=1)
+        self.srconvt2 = convt_bn_relu(128, 64, kernel_size, stride, 1, 1)
+
+        self.convt1 = convt_bn_relu(in_channels=64,
+                                    out_channels=64,
+                                    kernel_size=kernel_size,
+                                    stride=1,
+                                    padding=1)
+        self.srconvt1 = convt_bn_relu(64, 64, kernel_size, 1, 1)
+
+        self.convt2x = convt_bn_relu(in_channels=64,
+                                    out_channels=64,
+                                    kernel_size=kernel_size,
+                                    stride=2,
+                                    padding=1,
+                                    output_padding=1)
+        self.srconvt2x = convt_bn_relu(112, 64, kernel_size, 2, 1, 1)
+
+        self.convt4x = convt_bn_relu(in_channels=128,
+                                    out_channels=64,
+                                    kernel_size=kernel_size,
+                                    stride=2,
+                                    padding=1,
+                                    output_padding=1)
+        self.srconvt4x = convt_bn_relu(128, 64, kernel_size, 2, 1, 1)
+
+        self.DC_4x_to_1x = convt_bn_relu(64, 64, 4, 4, 0, 0)
+        self.SR_1x_to_4x = conv_bn_relu(64, 64, 4, 4, 0)
+
+
+        self.SR = conv_bn_relu(in_channels=128,
+                                   out_channels=64,
+                                   kernel_size=1,
+                                   stride=1,
+                                   bn=True,
+                                   relu=True)
+
+        self.DC = conv_bn_relu(in_channels=64,
+                                   out_channels=64,
+                                   kernel_size=1,
+                                   stride=1,
+                                   bn=True,
+                                   relu=True)
+
+        self.DC_out = conv_bn_relu(in_channels=128,
+                                   out_channels=1,
+                                   kernel_size=1,
+                                   stride=1,
+                                   bn=False,
+                                   relu=False)
+        self.SR_out = conv_bn_relu(in_channels=128,
+                                   out_channels=1,
+                                   kernel_size=1,
+                                   stride=1,
+                                   bn=False,
+                                   relu=False)
+        self.dc_and_sr_field_transform_4 = field_transform(256, 16, 76)
+        self.dc_and_sr_field_transform_3 = field_transform(128, 32, 152)
+        self.dc_and_sr_field_transform_2 = field_transform(64, 64, 304)
+        self.dc_and_sr_field_transform_1 = field_transform(64, 64, 304)
+
+    def forward(self, z):
+        # first layer
+        sparse = z['d']
+        rgb = z['rgb']
+
+        sparse = F.max_pool2d(sparse, 4, 4) # B, 1, 64, 304
+        # rgb = F.max_pool2d(rgb, 4, 4)
+
+        # sparse encoding ############################################################
+        conv1_d = self.conv1_d(sparse) # B, 16, 64, 304
+
+        # rgb encoding with cnn ######################################################
+        conv1_img = self.conv1_img(rgb)       # 1x
+        conv1_img = self.conv1_img2(conv1_img) # B, 64, 256, 1216
+
+        conv2_img = self.conv2_img(conv1_img) # 2x
+        conv2_img = self.conv2_img2(conv2_img) # B, 64, 128, 608
+
+        conv3_img = self.conv3_img(conv2_img) # 4x
+        conv3_img = self.conv3_img2(conv3_img) # B, 48, 64, 304
+
+
+        # encoder net ################################################################
+        conv1 = torch.cat((conv1_d, conv3_img), 1)  # B, 64, 64, 304
+        conv2 = self.conv2(conv1)  # B, 64, 64, 304
+        conv3 = self.conv3(conv2)  # B, 128, 32, 152
+        conv4 = self.conv4(conv3)  # B, 256, 16, 76
+        conv5 = self.conv5(conv4)  # B, 512, 8, 38
+
+
+
+        # DC and SR decoder ##################################################################
+        # 4
+        convt4 = self.convt4(conv5)  # B, 256, 16, 76
+        y = convt4 + 0.5 * conv4
+
+        sr_convt4 = self.srconvt4(conv5)  # B, 256, 16, 76
+        x = sr_convt4 + 0.5 * conv4
+
+        # field transform 4
+        # y, x = self.dc_and_sr_field_transform_4(y, x)
+        # 4 end
+
+        #************************************************#
+
+        # 3
+        convt3 = self.convt3(y)  # B, 128, 32, 152
+        y = convt3 + 0.5 * conv3
+
+        sr_convt3 = self.srconvt3(x)  # B, 128, 32, 152
+        x = sr_convt3 + 0.5 * conv3
+
+        # field transform 3
+        # y, x = self.dc_and_sr_field_transform_3(y, x)
+        # 3 end
+
+        # ************************************************#
+
+        # 2
+        convt2 = self.convt2(y)  # B, 64, 64, 304
+        y = convt2 + 0.5 * conv2
+
+        sr_convt2 = self.srconvt2(x)  # B, 64, 64, 304
+        x = sr_convt2 + 0.5 * conv2
+
+        # field transform 2
+        y, x = self.dc_and_sr_field_transform_2(y, x)
+        # 2 end
+
+        # ************************************************#
+
+        # 1
+        convt1 = self.convt1(y)   # B, 64, 64, 304
+        y = convt1 + 0.5 * conv1
+
+        sr_convt1 = self.srconvt1(x)  # B, 64, 64, 304
+        x = sr_convt1 + 0.5 * conv1
+
+        # field transform 1
+        y, x = self.dc_and_sr_field_transform_1(y, x)
+        # 1 end
+
+        # ************************************************#
+
+        DC_4x = self.DC(y)  # B, 64, 64, 304
+
+        DC_1x = self.DC_4x_to_1x(DC_4x)   # B, 64, 256, 1216
+
+        x = torch.cat((x, conv3_img), 1)
+        x = self.srconvt2x(x)  # B, 64, 128, 608
+
+        x = torch.cat((x, conv2_img), 1)
+        x = self.srconvt4x(x)  # B, 64, 256, 1216
+
+        x = torch.cat((x, conv1_img), 1)
+
+        x = self.SR(x) # B, 64, 256, 1216
+
+        SR_4x = self.SR_1x_to_4x(x)
+
+        SR_cat_DC = torch.cat((x, DC_1x), 1)
+        DC_cat_SR = torch.cat((DC_4x, SR_4x), 1)
+
+        DC_out = self.DC_out(DC_cat_SR)
+        SR_out = self.SR_out(SR_cat_DC)
+
+
+        if self.training:
+            return 100 * DC_out, 100 * SR_out
+        else:
+            min_distance = 0.9
+            return F.relu(100 * DC_out - min_distance) + min_distance, F.relu(100 * SR_out - min_distance) + min_distance
